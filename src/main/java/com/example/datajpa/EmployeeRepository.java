a4372bc72e13e25623f12c4677292d0552c739d9
fahad5359 <fabbgm5359@gmail.com>
2022-05-21 23:51:34 +0300

    Creating a Schema with JPA

--- src/main/java/com/example/datajpa/EmployeeRepository.java
+++ src/main/java/com/example/datajpa/EmployeeRepository.java
@@ -2,7 +2,11 @@ package com.example.datajpa;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 //  WE EXTENDED THE "CrudRepository" because we want to use the crud operations
 public interface EmployeeRepository extends CrudRepository <Employee, Long>{
-
+//    we create a method that do stuff for us, SpringData JPA dose allot of  good commands for us, and we dont need to
+//    Write our own query...
+        List<Employee>  fi
 }
